0b9f151cde53a4d3871ec8501a701cac024d7cfd
ZacharyANelson <47261023+ZacharyANelson@users.noreply.github.com>
2019-03-07 21:17:49 -0600

    Worked on it
    added some animation

---
+++ Assets/MoveBlocksAttachment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlocksAttachment : MonoBehaviour
+{
+    public GameObject MoveBlocks;
+    public GameObject ThePlayer;
+
+void OnTriggerEnter()
+    {
+        ThePlayer.transform.parent = MoveBlocks.transform;
+    }
+}
