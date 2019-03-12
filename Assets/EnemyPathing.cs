cff41662cc71dbcad8f80b8b2a1ad9f3b66c8e41
ADadBadJoke <46792735+ADadBadJoke@users.noreply.github.com>
2019-03-12 03:41:47 -0500

    updated game
    added enemy that chases player on second platform.  Added maze walls and goal location onto the second platform

---
+++ Assets/EnemyPathing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathing : MonoBehaviour
+{
+
+    public Transform player;
+
+    public NavMeshAgent nav;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
---
+++ Assets/MovingBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour
+{
+    public GameObject MoveBlocks;
+    public GameObject ThePlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "MovingPlatform")
+            {
+                ThePlayer.transform.parent = other.gameObject.transform;
+            }
+        }
+
+
+
+        void OnTriggerExit(Collider other)
+        {
+            ThePlayer.transform.parent = null;
+        }
+    }
+}
